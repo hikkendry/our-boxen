d0392cb61ed2403ed445cf1eca7fc844ce28c788
Rory Powis <rory@hikkendry.com>
2013-12-30 13:04:28 +0000

    rpowis: initial dotfiles module

--- modules/people/manifests/rpowis/dotfiles.pp
+++ modules/people/manifests/rpowis/dotfiles.pp
@@ -1,25 +1,33 @@
 class people::rpowis::dotfiles {
 
-  repository { '${boxen::config::srcdir}/dotfiles':
-    source => 'rpowis/dotfiles',
+  repository { "${boxen::config::srcdir}/dotfiles":
+    source => "rpowis/dotfiles"
   }
 
-  file { '${boxen::config::srcdir}/dotfiles/bootstrap.sh':
+  repository { "${boxen::config::srcdir}/rpowis-zshtheme":
+    source => "https://gist.github.com/3169614.git"
+  }
+
+  file { "boostrap perms":
+  	path => "${boxen::config::srcdir}/dotfiles/bootstrap.sh",
   	ensure => present,
-  	mode => 'u+x'
+  	mode => "u+x"
   }
 
-#	exec { "/Users/${boxen_user}/.zshrc":
-#	  ensure  => link,
-#	  mode    => '0644',
-#	  target  => "${boxen::config::srcdir}/dotfiles/.zshrc",
-#	  require => Repository["${boxen::config::srcdir}/dotfiles"],
-#	}
+	exec { "run bootstrap":
+		command => "sh ${boxen::config::srcdir}/dotfiles/bootstrap.sh -f",
+	  require => File["boostrap perms"]
+	}
 
-#  file { "/Users/${my_username}/.oh-my-zsh":
-#    ensure  => link,
-#    target  => "${boxen::config::srcdir}/oh-my-zsh",
-#    require => Repository["${boxen::config::srcdir}/oh-my-zsh"],
-#  }
+	file { "/Users/${boxen_user}/.oh-my-zsh/themes/rpowis.zsh-theme":
+		ensure => symlink,
+		target => "${boxen::config::srcdir}/rpowis-zshtheme/rpowis.zsh-theme",
+		require => Repository["${boxen::config::srcdir}/rpowis-zshtheme"]
+	}
+
+  # Set the shell
+#	exec { "sudo chsh -s /bin/zsh ${boxen_user}":
+#		require => Exec["run bootstrap"]
+#	}
 
 }
