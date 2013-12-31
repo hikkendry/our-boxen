bbe107a0d173881a436d740bf82e556e5e821217
Rory Powis <rory@hikkendry.com>
2013-12-31 02:34:53 +0000

    Move nodejs into hikkendry module

--- modules/hikkendry/manifests/environment.pp
+++ modules/hikkendry/manifests/environment.pp
@@ -8,6 +8,7 @@ class hikkendry::environment {
   include hikkendry::apache
   include hikkendry::php
   include hikkendry::ruby
+  # include hikkendry::node
 
   # applications
   include hikkendry::applications
