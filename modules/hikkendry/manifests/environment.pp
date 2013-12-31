57c6534c3313e27c8ac10d9170e98c72c14791ec
Rory Powis <rory@hikkendry.com>
2013-12-31 02:36:11 +0000

    Comment out all but (M)AMP and dnsmasq

--- modules/hikkendry/manifests/environment.pp
+++ modules/hikkendry/manifests/environment.pp
@@ -1,16 +1,23 @@
 class hikkendry::environment {
 	
 	# core modules
-  include ohmyzsh
-  include brewcask
+  # include ohmyzsh
+  # include brewcask
   
   # development
   include dnsmasq
   include hikkendry::apache
   include hikkendry::php
-  include hikkendry::ruby
+  # include hikkendry::ruby
   # include hikkendry::node
 
   # applications
-  include hikkendry::applications
+  # include hikkendry::applications
+
+  # iterm colour theme
+  # repository { "solarized":
+  # 	path => "${boxen::config::srcdir}/solarized",
+  #   source => "altercation/solarized"
+  # }
+
 }
